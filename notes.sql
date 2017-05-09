fa56472bcb4fc469cc66225069e616bbe3e58fc5
PuzzleShot <jonhlamb@gmail.com>
2017-05-09 18:05:17 -0500

    Update notes.sql
    add forgotten auto_increments

--- notes.sql
+++ notes.sql
@@ -1,6 +1,6 @@
 create table users
 (
-id bigint unsigned not null,
+id bigint unsigned not null auto_increment,
 name tinytext not null,
 password_hash text not null,
 primary key (id)
@@ -8,7 +8,7 @@ primary key (id)
 
 create table cats
 (
-id bigint unsigned not null,
+id bigint unsigned not null auto_increment,
 name tinytext not null,
 parent bigint unsigned not null default 0,
 primary key (id)
@@ -16,7 +16,7 @@ primary key (id)
 
 create table notes
 (
-id bigint unsigned not null,
+id bigint unsigned not null auto_increment,
 type enum("inq","sts","req","adv") not null,
 note tinytext not null,
 follows text,
