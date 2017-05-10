706332c1c6e437716b5f990c300caaa6c921ac3a
PuzzleShot <jonhlamb@gmail.com>
2017-05-10 09:52:34 -0500

    Update json.php
    add logic for requesting all info at once

--- json.php
+++ json.php
@@ -3,6 +3,20 @@ require_once("jonSQL.php");
 
 if(isset($_REQUEST["request"])){
     switch($_REQUEST["request"]){
+        case "both":
+            $cats = jonSQL::query("select * from cats");
+            $result = array();
+            array_push($result,array(),array());
+            for($i=0; $i<$cats[1]; $i++){
+                $cat = jonSQL::fetch_array($cats[0],MYSQLI_ASSOC);
+                array_push($result[0],$cat);
+            }
+            $notes = jonSQL::query("select * from notes");
+            for($i=0; $i<$notes[1]; $i++){
+                $note = jonSQL::fetch_array($notes[0],MYSQLI_ASSOC);
+                array_push($result[1],$note);
+            }
+            break;
         case "cats":
             $cats = jonSQL::query("select * from cats");
             if($cats[1] > 0){
