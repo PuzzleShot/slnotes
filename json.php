ea33817923e87d982780d299d038000816dc5d36
PuzzleShot <jonhlamb@gmail.com>
2017-05-10 20:22:15 -0500

    Update json.php

--- json.php
+++ json.php
@@ -9,11 +9,17 @@ if(isset($_REQUEST["request"])){
             array_push($result,array(),array());
             for($i=0; $i<$cats[1]; $i++){
                 $cat = jonSQL::fetch_array($cats[0],MYSQLI_ASSOC);
+                $cat["id"] = (int)$cat["id"];
+                $cat["parent"] = (int)$cat["parent"];
                 array_push($result[0],$cat);
             }
             $notes = jonSQL::query("select * from notes");
             for($i=0; $i<$notes[1]; $i++){
                 $note = jonSQL::fetch_array($notes[0],MYSQLI_ASSOC);
+                $note["follows"] = explode(",",$note["follows"]);
+                for($i=0; $i<count($note["follows"]); $i++){
+                    $note["follows"][$i] = (int)$note["follows"][$i];
+                }
                 array_push($result[1],$note);
             }
             break;
@@ -23,6 +29,8 @@ if(isset($_REQUEST["request"])){
                 $result = array();
                 for($i=0; $i<$cats[1]; $i++){
                     $cat = jonSQL::fetch_array($cats[0],MYSQLI_ASSOC);
+                    $cat["id"] = (int)$cat["id"];
+                    $cat["parent"] = (int)$cat["parent"];
                     array_push($result,$cat);
                 }
             }
@@ -32,6 +40,7 @@ if(isset($_REQUEST["request"])){
             $cat = jonSQL::query("select * from cats where id = ?",array($id));
             if($cat[1] == 1){
                 $result = jonSQL::fetch_array($cat[0],MYSQLI_ASSOC);
+                $result["int"] = (int)$result["int"];
             }
             break;
         case "notes":
@@ -40,6 +49,10 @@ if(isset($_REQUEST["request"])){
                 $result = array();
                 for($i=0; $i<$notes[1]; $i++){
                     $note = jonSQL::fetch_array($notes[0],MYSQLI_ASSOC);
+                    $note["follows"] = explode(",",$note["follows"]);
+                    for($i=0; $i<count($note["follows"]); $i++){
+                        $note["follows"][$i] = (int)$note["follows"][$i];
+                    }
                     array_push($result,$note);
                 }
             }
@@ -49,6 +62,10 @@ if(isset($_REQUEST["request"])){
             $note = jonSQL::query("select * from notes where id = ?",array($id));
             if($note[1] == 1){
                 $result = jonSQL::fetch_array($note[0],MYSQLI_ASSOC);
+                $result["follows"] = explode(",",$result["follows"]);
+                for($i=0; $i<count($result["follows"]); $i++){
+                    $result["follows"][$i] = (int)$result["follows"][$i];
+                }
             }
             break;
         default:
