6d620ac4b1d5d7e4ffd770662c8db442d990cdd3
PuzzleShot <jonhlamb@gmail.com>
2017-05-11 13:11:16 -0500

    Update json.php
    add appropriate code to make results into objects

--- json.php
+++ json.php
@@ -5,35 +5,38 @@ if(isset($_REQUEST["request"])){
     switch($_REQUEST["request"]){
         case "both":
             $cats = jonSQL::query("select * from cats");
-            $result = array();
-            array_push($result,array(),array());
+            $result = array("cats" => array(),"notes" => array());
             for($i=0; $i<$cats[1]; $i++){
                 $cat = jonSQL::fetch_array($cats[0],MYSQLI_ASSOC);
                 $cat["id"] = (int)$cat["id"];
                 $cat["parent"] = (int)$cat["parent"];
-                array_push($result[0],$cat);
+                array_push($result["cats"],$cat);
             }
             $notes = jonSQL::query("select * from notes");
             for($i=0; $i<$notes[1]; $i++){
                 $note = jonSQL::fetch_array($notes[0],MYSQLI_ASSOC);
+                $note["id"] = (int)$note["id"];
+                $note["cat"] = (int)$note["cat"];
                 $note["follows"] = explode(",",$note["follows"]);
                 for($i=0; $i<count($note["follows"]); $i++){
                     $note["follows"][$i] = (int)$note["follows"][$i];
                 }
-                array_push($result[1],$note);
+                array_push($result["notes"],$note);
             }
+            $result = (obj)$result;
             break;
         case "cats":
             $cats = jonSQL::query("select * from cats");
             if($cats[1] > 0){
-                $result = array();
+                $result = array("cats" => array());
                 for($i=0; $i<$cats[1]; $i++){
                     $cat = jonSQL::fetch_array($cats[0],MYSQLI_ASSOC);
                     $cat["id"] = (int)$cat["id"];
                     $cat["parent"] = (int)$cat["parent"];
-                    array_push($result,$cat);
+                    array_push($result["cats"],$cat);
                 }
             }
+            $result = (object)$result;
             break;
         case "cat":
             $id = jonSQL::prepare_parameter($_REQUEST["id"]);
@@ -42,31 +45,38 @@ if(isset($_REQUEST["request"])){
                 $result = jonSQL::fetch_array($cat[0],MYSQLI_ASSOC);
                 $result["int"] = (int)$result["int"];
             }
+            $result = (object)$result;
             break;
         case "notes":
             $notes = jonSQL::query("select * from notes");
             if($notes[1] > 0){
-                $result = array();
+                $result = array("notes" => );
                 for($i=0; $i<$notes[1]; $i++){
                     $note = jonSQL::fetch_array($notes[0],MYSQLI_ASSOC);
+                    $note["id"] = (int)$note["id"];
+                    $note["cat"] = (int)$note["cat"];
                     $note["follows"] = explode(",",$note["follows"]);
                     for($i=0; $i<count($note["follows"]); $i++){
                         $note["follows"][$i] = (int)$note["follows"][$i];
                     }
-                    array_push($result,$note);
+                    array_push($result["notes"],$note);
                 }
             }
+            $result = (object)$result;
             break;
         case "note":
             $id = jonSQL::prepare_parameter($_REQUEST["id"]);
             $note = jonSQL::query("select * from notes where id = ?",array($id));
             if($note[1] == 1){
                 $result = jonSQL::fetch_array($note[0],MYSQLI_ASSOC);
+                $result["id"] = (int)$result["id"];
+                $result["cat"] = (int)$result["cat"];
                 $result["follows"] = explode(",",$result["follows"]);
                 for($i=0; $i<count($result["follows"]); $i++){
                     $result["follows"][$i] = (int)$result["follows"][$i];
                 }
             }
+            $result = (object)$result;
             break;
         default:
             break;
