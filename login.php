09d1396dba2ca7df730c3a06356f4ce1ecf9d470
PuzzleShot <jonhlamb@gmail.com>
2017-05-16 17:00:55 -0500

    Create login.php

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+require_once("backend.php");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Log in</title>
+    </head>
+    <body>
+        <form method="post" action="">
+            <input type="text" name="clock_id">
+            <input type="password" name="password">
+        </form>
+    </body>
+</html>
