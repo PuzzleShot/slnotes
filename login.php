bad5ee73973f7b4b0386bc6e437b15b392af0691
PuzzleShot <jonhlamb@gmail.com>
2017-05-16 19:04:18 -0500

    Update login.php
    add basic failure message

--- login.php
+++ login.php
@@ -8,8 +8,14 @@ require_once("backend.php");
     </head>
     <body>
         <form method="post" action="">
+            <?php
+            if(isset($_SESSION["failure"]) && ($_SESSION[""] == true)){
+                echo "<p style='color:red'>Failed to log in.</p>";
+            }
+            ?>
             <input type="text" name="clock_id">
             <input type="password" name="password">
+            <input type="submit" value="login">
         </form>
     </body>
 </html>
