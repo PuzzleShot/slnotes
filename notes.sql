9f4e958e424b481bf2fce315ca3bdb3734cc6b98
PuzzleShot <jonhlamb@gmail.com>
2018-02-26 10:35:33 -0600

    Update notes.sql
    add types table

--- notes.sql
+++ notes.sql
@@ -17,10 +17,22 @@ primary key (id)
 create table notes
 (
 id bigint unsigned not null auto_increment,
+cat bigint unsigned not null,
 type enum("inq","sts","req","adv") not null,
 note tinytext not null,
 chat text not null,
 follows text,
+primary key (id),
+foreign key (cat) references cats(id)
+);
+
+create table types
+(
+id bigint unsigned not null auto_increment,
+cat bigint unsigned not null,
+name tinytext not null,
+type enum("inq","sts","req","adv") not null,
+note tinytext not null,
 primary key (id),
 foreign key (cat) references cats(id)
 );
